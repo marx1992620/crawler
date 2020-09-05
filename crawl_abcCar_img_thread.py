f0448176302cc2e4589eaf7e33ec16ef7fa34e6e
Ｍartin Cho <66872857+marx1992620@users.noreply.github.com>
2020-09-05 11:37:13 +0800

    Update crawl_abcCar_img_thread.py
    add logging

--- crawl_abcCar_img_thread.py
+++ crawl_abcCar_img_thread.py
@@ -12,8 +12,57 @@ import pandas as pd
 import json
 from queue import Queue
 import threading
+import logging
+
+logging.basicConfig(level=logging.INFO,
+    format='%(asctime)s - %(levelname)s : %(message)s',
+    datefmt='%Y%m%dT%H%M%S',filename='mylog.txt')
+logging.debug('debug')
+logging.info('info')
+logging.warning('warning')
+logging.error('error')
+logging.critical('critical')
 
 q = Queue()
+
+# 抓所有車網址
+def crawl_page():
+    # 讓driver等頁面內容5秒，超過5秒則跳過
+    # element = WebDriverWait(driver,5).until(expected_conditions.presence_of_element_located((By.CLASS_NAME,'abc-container')))
+    options = selenium.webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = selenium.webdriver.Chrome(chrome_options=options, executable_path='./chromedriver')
+    url='https://www.abccar.com.tw/search?tab=1&SearchType=1'
+    driver.get(url)
+    url_dict = {}
+
+    for page in range(1,11): # 設定爬取10頁
+        try:
+            # 若網頁資料5秒後還未出現，便跳過此頁
+            element = WebDriverWait(driver,5).until(expected_conditions.presence_of_element_located((By.XPATH,'/html/body/main/form/section[3]/section/div[2]/div[3]/div[3]/div[3]/button')))
+            time.sleep(3)
+
+            soup = BeautifulSoup(driver.page_source,'html.parser')
+            url_list = soup.select('div[class="row row--10 ng-scope"]')[0].select('a[itemprop="mainEntityOfPage"]')
+
+            for i in range(len(url_list)):
+                url = url_list[i]['href']
+                id = url[30:37]
+                if id in url_dict:
+                    pass
+                else:
+                    url_dict[id]=[url]
+            # 點擊下一頁，降低被網站重複推薦同物件
+            driver.find_element_by_xpath('/html/body/main/form/section[3]/section/div[2]/div[3]/div[3]/div[3]/button').click()
+        except Exception as e:
+            print(e,page)
+        except :
+            print("系統error",page)
+        print(f"第{page}頁")
+    print(len(url_dict))
+    js = json.dumps(url_dict)
+    with open(r'./abcCar_url.txt', 'w', encoding='utf-8') as f:
+        f.write(js)
 def crawl_img(url):
     options = selenium.webdriver.ChromeOptions()
     options.add_argument('--headless')
@@ -78,4 +127,16 @@ class Parser(threading.Thread): # 此為python繼承語法
             url = q.get() # 從Queue取網址
             crawl_img(url)
 
-crawl_img_thread()
+
+if __name__ == '__main__':
+    tStart = time.time() # 起始時間
+    logging.debug('debug')
+    logging.info('info')
+    logging.warning('warning')
+    logging.error('error')
+    logging.critical('critical')
+    print("start crawling url and content")
+    crawl_page()
+    crawl_img_thread()
+    tEnd = time.time() # 結束時間
+    print('Cost %d seconds' % (tEnd - tStart)) # 完成花費時間
