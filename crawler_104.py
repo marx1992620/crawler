ebfa29bf7baec8f37a8c3e02b43421c5ae788845
Martin <martin_cho@pegatroncorp.com>
2023-05-03 18:14:05 +0800

    modify crawler and add config

--- crawler_104.py
+++ crawler_104.py
@@ -12,8 +12,8 @@ from queue import Queue
 # import xlsxwriter
 # import re
 
-if not os.path.exists('./work'):
-    os.mkdir('./work')
+if not os.path.exists('./output'):
+    os.mkdir('./output')
 
 url_Queue = Queue()
 pages_Queue = Queue()
@@ -22,10 +22,10 @@ times_Queue = Queue()
 
 for tt in range(1000):
     times_Queue.put(tt) # 建立編號Queue
+
+skills = ['Linux', 'Python', 'Hadoop', 'MySQL','MongoDB', 'Kafka', 'R', 'ETL', 'Docker', 'Tableau', 'PowerBI', 'Spark','Machine Learning', 'AI', 'cloud']
 df = pd.DataFrame(columns=['company', 'job_name', 'job_content', 'job_exp', 'job_require', 'job_welfare',
-                           'job_contact', 'URL', 'Linux', 'Python', 'Hadoop', 'Mysql',
-                           'MongoDB', 'Kafka', 'R', 'ETL', 'Docker', 'Tableau', 'PowerBI', 'Spark',
-                           'Machine Learning', 'AI', 'cloud'])
+                           'job_contact', 'URL'] + skills)
 def crawl_url():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
     # print("請輸入關鍵字: ")
@@ -49,27 +49,26 @@ def crawl_content(url,headers):
     job_welfare = json_data['data']['welfare']['welfare']
     job_content = json_data['data']['jobDetail']['jobDescription']
     job_exp = "工作經驗:" + json_data['data']['condition']['workExp']
-    tech = [i['description'] for i in json_data['data']['condition']['specialty']]
+    job_skills = [i['description'] for i in json_data['data']['condition']['specialty']]
     # 讀取同義字檔案 並建同義字字典
     synonym_dict = {}
     with open(r'./mydict.txt', 'r', encoding='utf-8') as syn:
         syn_str = syn.read().split("\n")
     for each_row in syn_str:
         synonym_dict[each_row.split(',')[0]] = [item for item in each_row.split(',')]
-    # 搜索所需15個技能
-    c = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    # 配對技能要求: 0為沒有該要求 1為有要求
-    for a in tech:
-        t = 0
+    # 搜索所需技能
+    c = [0 for _ in range(len(skills))]
+    # times為配對技能要求次數
+    for job_skill in job_skills:
+        times = 0
         for b in synonym_dict.values(): # 從同義字字典匹配技能項
-            if a.lower() in b:
-                c[t] = 1
-            t += 1
+            if job_skill.lower() in b:
+                c[times] = 1
+            times += 1
     print(company,job_name)
 
     tt = times_Queue.get() # 從times Queue獲取編號 依序新增row資料
-    df.loc[tt] = [company, job_name, job_content, job_exp, job_require, job_welfare, job_contact, url, c[0],
-                     c[1], c[2], c[3],c[4], c[5], c[6], c[7], c[8], c[9], c[10], c[11], c[12], c[13], c[14]]
+    df.loc[tt] = [company, job_name, job_content, job_exp, job_require, job_welfare, job_contact, url] + c
     time.sleep(random.randint(3,5)) # 每爬完一頁休息3~5秒
     df.to_csv(r'./work/craw_104.csv', index=0,encoding='utf-8-sig') # 轉成CSV檔
     df.to_excel(r'./work/craw_104.xlsx', engine='xlsxwriter') # 轉成xlsx檔
